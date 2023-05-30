15ce11a8f5b605b4a90a30bb57e72032bc769415
Ummadi, Mani Kanta Reddy <ManiKantaReddy_Ummadi@comcast.com>
2023-05-30 15:04:41 +0530

    initial commit bootstrapping the project

---
+++ src/test/java/com/neon/jwtsecurity/JwtSecurityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.neon.jwtsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+}
